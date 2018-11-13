31870ec2b9cb10a52c1d0516244cc09d113d81bc
Shivang Agarwal <shivangagarwal98@gmail.com>
2018-11-13 20:59:28 +0530

    add views

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.contrib.auth import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'', include('blog.urls')),
+    path('accounts/login/', views.login, name='login'),
+    path('accounts/logout/', views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
